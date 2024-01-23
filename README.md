372889eac82cb4f07e728eeb5e85bea47f87d7ef
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-01-23 09:34:33 -0300

    ✨ first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RF
+
+- [X] O usuário deve poder criar uma nova transação;
+- [X] O usuário deve poder obter um resumo da sua conta;
+- [X] O usuário deve poder listar todas transações que já ocorreram;
+- [X] O usuário deve poder visualizar uma transação única;
+
+# RN
+
+- [X] A transação pode ser do tipo crédito que somará ao valor total, ou débito subtrairá;
+- [ ] Deve ser possível identificarmos o usuário entre as requisições;
+- [ ] O usuário só pode visualizar transações o qual ele criou;
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(cookie)
+
+app.addHook('preHandler', async (request, reply) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
